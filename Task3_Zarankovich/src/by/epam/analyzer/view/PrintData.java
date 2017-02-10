5694caa874e350ef8d3acfee5a51e20e035e0e4d
Archidel <archangel.albert.6@gmail.com>
2017-02-10 09:46:22 +0200

    add private constructor

--- Task3_Zarankovich/src/by/epam/analyzer/view/PrintData.java
+++ Task3_Zarankovich/src/by/epam/analyzer/view/PrintData.java
@@ -2,7 +2,7 @@ package by.epam.analyzer.view;
 
 public final class PrintData {
 	
-	public PrintData() {}
+	private PrintData() {}
 	
 	public static void printResponse(String response){
 		System.out.println(response);
