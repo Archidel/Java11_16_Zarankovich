bdc42ff1324495169e4e506d68f05d2433925601
Archidel <archangel.albert.6@gmail.com>
2017-02-10 09:40:06 +0200

    add private constructors

--- Task3_Zarankovich/src/by/epam/analyzer/service/validation/ValidationData.java
+++ Task3_Zarankovich/src/by/epam/analyzer/service/validation/ValidationData.java
@@ -2,6 +2,8 @@ package by.epam.analyzer.service.validation;
 
 public final class ValidationData {
 	
+	private ValidationData() {}
+	
 	public static boolean validationString(String line){		
 		if((line == null) || (line.isEmpty())){
 			return false;
--- Task3_Zarankovich/src/by/epam/analyzer/view/PrintData.java
+++ Task3_Zarankovich/src/by/epam/analyzer/view/PrintData.java
@@ -2,6 +2,8 @@ package by.epam.analyzer.view;
 
 public final class PrintData {
 	
+	public PrintData() {}
+	
 	public static void printResponse(String response){
 		System.out.println(response);
 	}
